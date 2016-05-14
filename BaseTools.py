7195b4b26ed581c356a2e622d83e07bc3aa94591
mhbashari <mhbm136900>
2016-05-15 00:15:22 +0430

    using scikit TNSE build the main structure of microlib

--- BaseTools.py
+++ BaseTools.py
@@ -1,11 +1,13 @@
-import numpy as np
 from bidi.algorithm import get_display
 import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+from gensim.models.word2vec import Word2Vec
+
+import arabic_reshaper
+from sklearn.manifold import TSNE
 
 matplotlib.rc('font', family='KacstOne')
-
-import matplotlib.pyplot as plt
-import arabic_reshaper
 
 
 class WordVisualizationBase2d:
@@ -16,23 +18,31 @@ class WordVisualizationBase2d:
         if word_dict is not None:
             self.word_dict = word_dict
             data = [item for key, item in word_dict.items()]
-            self.labels = list(self.words_process(word_dict.keys()))
-            print(data)
+            self.labels = list(self.words_arabic_reshape(word_dict.keys()))
+            # print(data)
             self.X = [elem[0] for elem in data]
             self.Y = [elem[1] for elem in data]
             self.colors = [index / 1000 for index in range(len(self.X))]
             self.size = 50
         elif kwargs is not None:
             print("You did not set as list")
-            self.labels = kwargs["labels"]
+            self.labels = self.words_arabic_reshape(kwargs["labels"])
 
             self.X = kwargs["X"]
-            print(self.X)
+            # print(self.X)
             self.Y = kwargs["Y"]
 
+    def get_X(self):
+        print("get_X->", self.X)
+        return self.X
 
+    def get_Y(self):
+        return self.Y
 
-    def words_process(self, words):
+    def get_labels(self):
+        return self.labels
+
+    def words_arabic_reshape(self, words):
         return map(lambda x: self.word_process(x), words)
 
     def show_plot(self):
@@ -48,3 +58,28 @@ class WordVisualizationBase2d:
     def word_process(self, x):
         reshaped_text = arabic_reshaper.reshape(x)
         return get_display(reshaped_text)
+
+
+class W2VPersianVis:
+    def __init__(self, w2v_model_path, selected_words=None):
+        self.model_path = w2v_model_path
+        self.model = Word2Vec.load(self.model_path)
+        self.selected_words = selected_words
+
+    def show_plot(self):
+        self.reduce_dimentionality()
+        if self.selected_words is None:
+            self.plt = WordVisualizationBase2d(X=self.reduced_vectors[:, 0], Y=self.reduced_vectors[:, 1],
+                                               labels=self.model.vocab.keys())
+        else:
+            self.plt = WordVisualizationBase2d(X=self.reduced_vectors[:, 0], Y=self.reduced_vectors[:, 1],
+                                               labels=self.selected_words)
+        self.plt.show_plot()
+
+    def reduce_dimentionality(self):
+        self.vectors = []
+        for key in self.selected_words:
+            self.vectors.append(self.model[key])
+        tnse_model = TSNE(n_components=2, random_state=0)
+        np.set_printoptions(suppress=True)
+        self.reduced_vectors = tnse_model.fit_transform(self.vectors)
--- tut.py
+++ tut.py
@@ -1,9 +1,33 @@
-from PersianTNSE import WordVisulizationTSNE
-def test_word_visualization():
-    print("++")
-    # wv = BaseTools.WordVisualizationBase2d({"سیب": (1, 2), "موز": (1, 3), "گربه": (4, 7)})
-    wv = WordVisulizationTSNE(["سیب", "گلابی", "هلو"], [(1, 1), (2, 2), (2, 2)], [1, 2, 3])
-    wv.show_plot()
+from gensim.models.word2vec import LineSentence
+from hazm import Normalizer, sent_tokenize, word_tokenize
+from gensim.models import word2vec
+
+from BaseTools import W2VPersianVis
 
 
-test_word_visualization()
+def sentences(file="simple_text"):
+    normalizer = Normalizer()
+    for line in open(file, "r", encoding="utf-8").readlines():
+        for sent in sent_tokenize(line):
+            yield word_tokenize(line)
+
+
+def create_word2vec(plain_text):
+    model = word2vec.Word2Vec(sentences=list(sentences()), size=50, window=5, min_count=0)
+    model.save("basica_persian_model.model")
+
+
+def test_word_visualization(model_path, some_words):
+    normalizer = Normalizer()
+    model = word2vec.Word2Vec.load(model_path)
+    vectors = [model[normalizer.normalize(word)] for word in some_words if
+               normalizer.normalize(word) in model.vocab.keys()]
+    # print(model[normalizer.normalize('فرهنگ')])
+    # print(model.similarity('فرهنگ', 'تمدن'))
+    # print(vectors)
+    rd = W2VPersianVis(model_path, selected_words=some_words)
+    rd.show_plot()
+
+
+# create_word2vec(open("simple_text", "r", encoding="utf-8").read())
+test_word_visualization("basica_persian_model.model", ["حقوق", "قائل", "علامه", "تمدن"])
